664348c3f46ad7469d9d0924a1c4a448d85be52a
Nonoy Suguitan <nonoy.suguitan@gmail.com>
2021-03-06 23:19:43 -0800

    implement ingesting comments via praw module

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 import pandas
 import praw
@@ -38,26 +39,62 @@ def authenticate():
 
     return reddit
 
+def df_from_response(res):
+    # initialize temp dataframe for batch of data in response
+    df = pandas.DataFrame()
+
+    # loop through each post pulled from res and append to df
+    for post in res.json()['data']['children']:
+        df = df.append({
+            'subreddit': post['data']['subreddit'],
+            'title': post['data']['title'],
+            'selftext': post['data']['selftext'],
+            'upvote_ratio': post['data']['upvote_ratio'],
+            'ups': post['data']['ups'],
+            'downs': post['data']['downs'],
+            'score': post['data']['score'],
+            'link_flair_css_class': post['data']['link_flair_css_class'],
+            'created_utc': datetime.fromtimestamp(post['data']['created_utc']).strftime('%Y-%m-%dT%H:%M:%SZ'),
+            'id': post['data']['id'],
+            'kind': post['kind']
+        }, ignore_index=True)
+
+    return df
+
 def main():
+    # authenticate and initialize praw
     reddit = authenticate()
 
-    #reponse = requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
-
-    data = pandas.DataFrame()
+    # initialize data frames
+    reddit_posts = pandas.DataFrame()
     params = {'limit': 5}
 
-#    response = requests.get("https://oauth.reddit.com/r/CryptoCurrency/new",
-#                       headers=headers,
-#                       params=params)
+    res = requests.get("https://oauth.reddit.com/r/CryptoCurrency/new",
+                       headers=headers,
+                       params=params)
+
+    new_df = df_from_response(res)
+
+    reddit_posts = reddit_posts.append(new_df, ignore_index=True)
 
 #    response = requests.get("https://oauth.reddit.com/r/CryptoCurrency/comments/lysxbc",
 #                       headers=headers)
 
 #    print(json.dumps(response.json(), indent=4))
 
+    print(reddit_posts)
     print(f"{headers} hello world")
-    print(reddit.read_only)
 
+    for index, rows in reddit_posts.iterrows():
+        print(rows.id)
+        submission = reddit.submission(id=rows.id)
+
+        submission.comments.replace_more(limit=None)
+        for comment in submission.comments.list():
+            print(comment.body)
+            print("-----------")
+
+    print(f"{headers} hello world")
 
 if __name__ == "__main__":
     main()
